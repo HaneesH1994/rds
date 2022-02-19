ea3842a33fc63ccb93b1664a50f3a3c8c1bbefa4
HaneesH1994 <94160411+HaneesH1994@users.noreply.github.com>
2022-02-19 12:53:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ resource "aws_db_instance" "default" {
   engine_version       = var.version1
   instance_class       = var.class
   db_name              = var.name
-  vpc_id               = var.vpc
   username             = var.username
   password             = var.password
   parameter_group_name = "default.postgresql13.4"
