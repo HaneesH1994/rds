4bcad13dbdb8d129f01306d6b5c1e7e797b576c4
HaneesH1994 <94160411+HaneesH1994@users.noreply.github.com>
2022-02-19 12:46:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,11 @@ resource "aws_db_instance" "default" {
   engine               = var.engine
   engine_version       = var.version1
   instance_class       = var.class
-  name                 = var.name
+  db_name              = var.name
+  vpc_id               = var.vpc
   username             = var.username
   password             = var.password
-  parameter_group_name = "default.mysql5.7"
+  parameter_group_name = "default.postgresql13.4"
   deletion_protection = true
   skip_final_snapshot  = true
   
